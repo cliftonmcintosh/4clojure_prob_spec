21ca4a81fb14e82aae83c2db8536ab35f4f31b90
Clifton McIntosh <cliff.mcintosh@gmail.com>
2015-11-11 11:49:09 -0600

    solution for implementing range.

---
+++ spec/prob_spec/range_spec.clj
@@ -0,0 +1,13 @@
+(ns prob-spec.range-spec
+  (:require [speclj.core :refer :all]
+            [prob-spec.range :refer :all]))
+
+(describe "my-range"
+          (it "should make a sequence of positive numbers starting with one"
+              (should= '(1 2 3) (my-range 1 4)))
+          (it "should make a sequence of numbers when starting with a negative number"
+              (should= '(-2 -1 0 1) (my-range -2 2)))
+          (it "should make a sequence of numbers when starting with a positive number greater than one"
+              (should= '(5 6 7) (my-range 5 8))))
+
+(run-specs)
---
+++ src/prob_spec/range.clj
@@ -0,0 +1,8 @@
+(ns prob-spec.range)
+
+(defn my-range
+  ([start end] (my-range start end []))
+  ([start end accum]
+    (if (>= start end)
+      accum
+      (recur (inc start) end (conj accum start)))))
