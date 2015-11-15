e0e96a00bab9f27cd39bac9b3fcc8b8ca845a053
Clifton McIntosh <cliff.mcintosh@gmail.com>
2015-11-15 11:12:39 -0600

    solution for interpose.

---
+++ spec/prob_spec/interpose_spec.clj
@@ -0,0 +1,13 @@
+(ns prob-spec.interpose-spec
+  (:require [speclj.core :refer :all]
+            [prob-spec.interpose :refer :all]))
+
+(describe "my-interpose"
+         (it "should interpose numbers"
+             (should= [1 0 2 0 3] (my-interpose 0 [1 2 3])))
+          (it "should interpose strings"
+              (should= "one, two, three" (apply str (my-interpose ", " ["one" "two" "three"]))))
+          (it "should interpose keywords"
+              (should= [:a :z :b :z :c :z :d] (my-interpose :z [:a :b :c :d]))))
+
+(run-specs)
---
+++ src/prob_spec/interpose.clj
@@ -0,0 +1,11 @@
+(ns prob-spec.interpose)
+
+(defn my-interpose
+  [interposer coll]
+  (loop [x interposer
+        xs coll
+        accum []]
+    (cond
+      (empty? xs) accum
+      (= (count xs) 1) (conj accum (first xs))
+      :else (recur x (rest xs) (conj accum (first xs) x)))))
