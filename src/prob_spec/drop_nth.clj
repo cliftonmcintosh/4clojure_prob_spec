b57d965a711a904cbee1d34ba465c9382c2f4d89
Clifton McIntosh <cliff.mcintosh@gmail.com>
2015-11-21 14:13:30 -0600

    even more thread-last macro in the solution for drop-every-nth.

--- src/prob_spec/drop_nth.clj
+++ src/prob_spec/drop_nth.clj
@@ -1,11 +1,13 @@
 (ns prob-spec.drop-nth)
 
-
 (defn drop-every-nth
   [original index]
-  (let [indexed-items (map  #(conj [] (inc (first %)) (second %)) (map-indexed vector original))]
+  (let [indexed-items (->> original
+                           (map-indexed vector)
+                           (map #(conj [] (inc (first %)) (second %))))]
     (->> indexed-items
-      (filter (fn [input]
-                (not= 0 (rem (first input) index))))
-      (reduce #(conj %1 (take-last 1 %2)) [])
-      (flatten))))
+         (filter (fn [input]
+                   (not= 0 (rem (first input) index))))
+         (reduce #(conj %1 (take-last 1 %2)) [])
+         (flatten)
+         )))
