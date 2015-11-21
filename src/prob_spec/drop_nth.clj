27040868cb9d95beaeb03c6ae708e6995e4bce25
Clifton McIntosh <cliff.mcintosh@gmail.com>
2015-11-21 14:12:33 -0600

    use thread last in drop-every-nth.

--- src/prob_spec/drop_nth.clj
+++ src/prob_spec/drop_nth.clj
@@ -2,11 +2,10 @@
 
 
 (defn drop-every-nth
-  [coll index]
-  (let [indexed-items (map  #(conj [] (inc (first %)) (second %)) (map-indexed vector coll))]
-    (flatten (reduce
-               #(conj %1 (take-last 1 %2)) []
-               (filter (fn [input]
-                         (not= 0 (rem (first input) index)))
-                       indexed-items)))))
-
+  [original index]
+  (let [indexed-items (map  #(conj [] (inc (first %)) (second %)) (map-indexed vector original))]
+    (->> indexed-items
+      (filter (fn [input]
+                (not= 0 (rem (first input) index))))
+      (reduce #(conj %1 (take-last 1 %2)) [])
+      (flatten))))
